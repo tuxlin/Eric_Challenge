10ce2a72191fbed53cf0159c18835637ea73c87a
Eric <eric@pop-os.localdomain>
2021-10-10 02:53:16 -0500

    added setup script to ease zappa setup

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+python3.8 --version &> /dev/null
+
+if [ $? != 0 ]; then
+	echo "Please install python3.8 on your local machine to proceed"
+	exit 1
+fi
+
+echo "creating python3.8 virtual environment..."
+python3.8 -m venv venv
+
+echo "activating python3.8 virtual environment..."
+source ./venv/bin/activate
+
+echo "updating pip3.8..."
+pip install -U pip > /dev/null
+
+echo "installing necessary package requirements..."
+pip install -r ./requirements.in > /dev/null
+
+echo "done."
+echo
+
+echo "Please run the following commands and answer the prompts:"
+echo
+echo "source venv/bin/activate"
+echo "zappa init"
+echo
+echo
+echo "Once initialization is complete, run the next command:"
+echo
+echo "zappa deploy <stage-name>"
